7a46da219effe50205fcdff577536ead821410a5
kwatters <kwatters@tagged.com>
2012-08-01 16:08:14 -0700

    Make hooks in all/ get run.

---
+++ all/pre-receive/0-update-hooks.git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+#
+# Make server repositories receiving pushes first update their hooks directory.
+#
+
+GIT_HOOKS_DIR=`git rev-parse --git-dir`/hooks
+echo "Updating hooks in $GIT_HOOKS_DIR..."
+pushd $GIT_HOOKS_DIR
+git pull
+popd
